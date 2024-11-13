67644a26b8da867e80b4fc46b7181ede1ae6a256
Chelzea Salvador <chelzeasalvador@gmail.com>
2024-11-13 13:22:56 +0800

    fix build error

--- src/js/config.js
+++ src/js/config.js
@@ -1,8 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = process.env.API_KEY;
+export const KEY = "35de0df9-067a-4472-a3e9-12ef3a34dcaa";
 export const MODAL_CLOSE_SEC = 2.5;
