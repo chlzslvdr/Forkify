51e07a759d68821f7b9e8952230e20ce762ab504
Chelzea Salvador <chelzeasalvador@gmail.com>
2024-11-13 13:40:21 +0800

    fix build error in fractional

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = "35de0df9-067a-4472-a3e9-12ef3a34dcaa";
+export const KEY ='35de0df9-067a-4472-a3e9-12ef3a34dcaa';
 export const MODAL_CLOSE_SEC = 2.5;
